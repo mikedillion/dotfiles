1a7c2b6806b0f504f6fac6b80fc697db7f34ff91
Mike Dillion <mike.dillion@gmail.com>
2015-01-27 06:01:47 +0000

    Not just osx

--- ruby.sh
+++ ruby.sh
@@ -1,7 +1,7 @@
 alias be='bundle exec'
 
 alias rr='rbenv rehash'
-source /Users/miked/.rbenv/completions/rbenv.bash
+source $HOME/.rbenv/completions/rbenv.bash
 
 #function rake {
 #  if [ -f Gemfile ]; then
