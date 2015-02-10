f1696b57535ebed9ea01758fc9dda0d31eca6258
Mike Dillion <mike.dillion@gmail.com>
2015-02-10 16:31:36 -0500

    Here's two dumb things

--- virtualbox.sh
+++ virtualbox.sh
@@ -1,7 +1,5 @@
 function vms(){
-  tput setaf 0 && tput setab 6 && echo "Running:" && tput reset;
   VBoxManage list runningvms
-  tput setaf 0 && tput setab 6 && echo "ALL VMs:" && tput reset;
   VBoxManage list vms;
 }
 
