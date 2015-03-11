1d541adb9bee77212f2290f95a79cfb10ecf858a
Mike Dillion <mike.dillion@gmail.com>
2015-03-11 15:55:31 -0400

    Let aws-cli use SSL again.

--- aws.sh
+++ aws.sh
@@ -1,5 +1,5 @@
 # AWS CLI
-alias aws='aws --no-verify-ssl'
+#alias aws='aws --no-verify-ssl'
 complete -C aws_completer aws
 export AWS_CONFIG_FILE=$HOME/dotfiles_private/aws/credentials
 #export AWS_DEFAULT_PROFILE='me'
