eae56b5d58030140b03513cec7476dd7e278ff33
Mike Dillion <mike.dillion@gmail.com>
2018-09-26 14:57:38 -0500

    oh, some java and spark stuff

--- source/java.sh
+++ source/java.sh
@@ -1,1 +1,4 @@
-export JAVA_HOME="/Library/Java/Home"
+# Print full JVM list with architectures.
+# /usr/libexec/java_home --verbose
+
+export JAVA_HOME=$(/usr/libexec/java_home -v 1.8.0_181)
--- source/spark.sh
+++ source/spark.sh
@@ -1,11 +1,15 @@
-export SPARK_HOME='/usr/local/Cellar/apache-spark/current'
+export SPARK_HOME="/usr/local/Cellar/apache-spark/current"
 export PATH="$PATH:$SPARK_HOME/bin"
 
-export SPARK_MASTER_IP="localhost"
+# export SPARK_MASTER_IP="127.0.0.1"
+export SPARK_LOCAL_IP="localhost"
 export SPARK_WORKER_MEMORY="4g"
 
-export PYSPARK_PYTHON="$VIRTUAL_ENV/bin/python"
+export PYSPARK_PYTHON="python"
+# export PYSPARK_PYTHON="$VIRTUAL_ENV/bin/python"
+
 # export PYSPARK_DRIVER_PYTHON="$VIRTUAL_ENV/bin/ipython"
 export PYSPARK_DRIVER_PYTHON="jupyter"
+
 export PYSPARK_DRIVER_PYTHON_OPTS="notebook"
-export PYSPARK_SUBMIT_ARGS='--master local[2] pyspark-shell'
+export PYSPARK_SUBMIT_ARGS="--master local[*] pyspark-shell --conf spark.driver.host=localhost"
