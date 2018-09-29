9618b485f51ac6c49e321904e5193c6c3d470614
Mike Dillion <mike.dillion@gmail.com>
2018-09-29 13:36:33 +0000

    Merge in some hadoop, spark, java stuff you've been carrying around

---
+++ source/hadoop.sh
@@ -0,0 +1,10 @@
+export HADOOP_PREFIX="/opt/hadoop"
+export HADOOP_CONF_DIR="$HADOOP_PREFIX/etc/hadoop"
+
+export JAVA_LIBRARY_PATH="$HADOOP_PREFIX/lib/native"
+export LD_LIBRARY_PATH="$HADOOP_PREFIX/lib/native"
+
+export PATH="$PATH:$HADOOP_PREFIX/bin:$HADOOP_PREFIX/sbin"
+
+export LIVY_PREFIX="/opt/livy/"
+export PATH="$PATH:$LIVY_PREFIX/bin"
--- source/java.sh
+++ source/java.sh
@@ -1,4 +1,7 @@
-# Print full JVM list with architectures.
-# /usr/libexec/java_home --verbose
-
-export JAVA_HOME=$(/usr/libexec/java_home -v 1.8.0_181)
+if [[ "$(uname)" == 'Darwin' ]]; then
+  # Print full JVM list with architectures.
+  # /usr/libexec/java_home --verbose
+  export JAVA_HOME=$(/usr/libexec/java_home -v 1.8.0_181)
+elif [[ "$(uname)" == 'Linux' ]]; then
+  export JAVA_HOME="/usr/lib/jvm/java-8-oracle"
+fi
