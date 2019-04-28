ab7dec0ff59be19352da603b3b9b466b29e5ca3c
Mike Dillion <mike.dillion@gmail.com>
2019-04-28 19:49:22 -0400

    Signed-off-by: Mike Dillion <mike.dillion@gmail.com>

--- source/postgres.sh
+++ source/postgres.sh
@@ -1,2 +1,2 @@
-export PATH="/Applications/Postgres.app/Contents/Versions/latest/bin:$PATH"
+export PATH="/Applications/Postgres.app/Contents/Versions/10/bin:$PATH"
 # export PGDATA=/usr/local/var/postgres
