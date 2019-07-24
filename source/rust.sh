b81c913aefc97cd339e0bdcd55979b7db18cc81b
Mike Dillion <mike.dillion@gmail.com>
2019-07-24 05:20:24 -0400

    Signed-off-by: Mike Dillion <mike.dillion@gmail.com>

--- source/rust.sh
+++ source/rust.sh
@@ -1,5 +1,5 @@
 # https://github.com/rust-lang/cargo
 # https://doc.rust-lang.org/cargo/getting-started/installation.html
-export PATH="$HOME/.cargo/bin:$PATH"
 
 source $HOME/.cargo/env
+# Which contains: export PATH="$HOME/.cargo/bin:$PATH"
