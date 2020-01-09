0055a16073001ea05c39a81f8610ee804aad225a
Mike Dillion <mike.dillion@gmail.com>
2020-01-09 00:19:41 -0500

    Signed-off-by: Mike Dillion <mike.dillion@gmail.com>

--- source/mandarin.sh
+++ source/mandarin.sh
@@ -1,10 +1,29 @@
 # https://github.com/tesseract-ocr/tessdata_best
 # https://github.com/tesseract-ocr/tesseract/wiki/Data-Files
-tesseract_cn() {
+tesseract_zh() {
   tesseract --tessdata-dir /usr/local/share/tessdata -l chi_sim "$@" stdout
 }
 
-# https://github.com/soimort/translate-shell
-trans_cn() {
-  trans -source zh-cn $lines
+docr() {
+  tesseract --tessdata-dir /usr/local/share/tessdata -l chi_sim "$HOME/Desktop/Screen\ *" stdout
 }
+
+# https://github.com/soimort/translate-shell
+# Note: pipe stdin to these
+trans_zn_to_en() {
+  trans -source zh-cn
+}
+
+trans_en_to_zh() {
+  trans :zh-cn
+}
+
+pbzhen() {
+  pbpaste | trans -source zh-cn
+}
+
+pbenzh() {
+  pbpaste | trans :zh-cn
+}
+
+alias ocr_desktop="tesseract_zh $HOME/Desktop/Screen\ * | trans_zn_to_en"
