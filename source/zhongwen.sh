2a964fab347a7695fb391d3d137661fc2fc74e10
Mike Dillion <mike.dillion@gmail.com>
2020-04-03 15:01:59 -0400

    Signed-off-by: Mike Dillion <mike.dillion@gmail.com>

--- source/zhongwen.sh
+++ source/zhongwen.sh
@@ -3,7 +3,7 @@
 # #############
 #
 # https://github.com/tesseract-ocr/tessdata_best
-# https://github.com/tesseract-ocr/tesseract/wiki/Data-Files
+# https://tesseract-ocr.github.io/tessdoc/Data-Files
 #
 # tesseract --list-langs
 #   List of available languages (5):
@@ -18,8 +18,10 @@
 #     7    Treat the image as a single text line.
 #   ...
 
+export TESSDATA_PREFIX='/usr/local/share/tessdata'
+
 tesseract_zh() {
-  tesseract --psm 7 --tessdata-dir /usr/local/share/tessdata -l chi_sim "$@" stdout | tr -d ' '
+  tesseract --psm 7 -l chi_sim "$@" stdout | tr -d ' '
 }
 
 
