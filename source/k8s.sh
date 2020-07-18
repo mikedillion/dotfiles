68d519ace0a6b39d23313079da185f5c644e8c81
Mike Dillion <mike.dillion@gmail.com>
2020-07-18 09:31:05 -0400

    Signed-off-by: Mike Dillion <mike.dillion@gmail.com>

--- source/k8s.sh
+++ source/k8s.sh
@@ -1,3 +1,13 @@
-function kt {
-  kubectl -n kube-system describe secret $(kubectl -n kube-system get secret | grep admin-user | awk '{print $1}') | grep "^token:" | sed -e 's/^token: *//' | pbcopy
+k8s_admin_token() {
+	admin_token="$(kubectl -n kube-system get secret | grep admin-user | awk '{print $1}')"
+
+  kubectl -n kube-system \
+		describe secret "$admin_token" \
+		| grep "^token:" \
+		| sed -e 's/^token: *//' \
+		| pbcopy
+}
+
+k8s_current_ns() {
+	echo "$(kubectl config view --minify --output 'jsonpath={..namespace}')"
 }
